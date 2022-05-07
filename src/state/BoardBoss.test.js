4c2f24ee3319ef3965dd20da328cae91aa6ec46b
Sean Spearman <smspearmandev@gmail.com>
2022-05-07 02:17:34 -0700

    Update BoardBoss tests

--- src/state/BoardBoss.test.js
+++ src/state/BoardBoss.test.js
@@ -1,11 +1,6 @@
-import { COLUMNS, ROWS } from "../constants";
-import BoardBoss, {
-  SCARAB,
-  EMPTY,
-  SELECTED,
-  PHARAOH,
-  InvalidMoveError,
-} from "./BoardBoss";
+import { COLUMNS, PLAYER_ONE, ROWS } from "../constants";
+import BoardBoss, { SCARAB, PHARAOH, InvalidMoveError } from "./BoardBoss";
+import PieceBoss from "./PieceBoss";
 
 it("builds an empty 10 x 8 board", () => {
   let boardBoss = new BoardBoss();
@@ -16,45 +11,52 @@ it("builds an empty 10 x 8 board", () => {
 
 it("can create a pharaoh at 2,3", () => {
   let boardBoss = new BoardBoss();
-  boardBoss.writeSpace([2, 3], PHARAOH);
+  const piece = new PieceBoss({ type: PHARAOH, player: PLAYER_ONE });
+  boardBoss.writeSpace([2, 3], piece);
 
-  expect(boardBoss.readSpace([2, 3])).toEqual(PHARAOH);
+  expect(boardBoss.readSpace([2, 3]).id).toEqual(piece.id);
 });
 
 it("can select pharaoh", () => {
   let boardBoss = new BoardBoss();
-  boardBoss.writeSpace([2, 3], PHARAOH);
+  const piece = new PieceBoss({ type: PHARAOH, player: PLAYER_ONE });
+  boardBoss.writeSpace([2, 3], piece);
 
   boardBoss.selectPiece([2, 3]);
 
-  expect(boardBoss.readSpace([2, 3])).toEqual(`${SELECTED}${PHARAOH}`);
+  expect(boardBoss.selectedPieceId).toEqual(piece.id);
 });
 
 it("can deselect pharaoh", () => {
   let boardBoss = new BoardBoss();
-  boardBoss.writeSpace([2, 3], `${SELECTED}${PHARAOH}`);
+  const piece = new PieceBoss({ type: PHARAOH, player: PLAYER_ONE });
+  boardBoss.writeSpace([2, 3], piece);
+  boardBoss.selectPiece([2, 3]);
 
   boardBoss.deselectPiece([2, 3]);
 
-  expect(boardBoss.readSpace([2, 3])).toEqual(PHARAOH);
+  expect(boardBoss.selectedPieceId).toEqual(null);
 });
 
 it("can move pharaoh from 2,3 to 3,3", () => {
   let boardBoss = new BoardBoss();
-  boardBoss.writeSpace([2, 3], PHARAOH);
+  const piece = new PieceBoss({ type: PHARAOH, player: PLAYER_ONE });
+  boardBoss.writeSpace([2, 3], piece);
 
   boardBoss.selectPiece([2, 3]);
   boardBoss.movePiece([2, 3], [3, 3]);
 
-  expect(boardBoss.readSpace([2, 3])).toEqual(EMPTY);
-  expect(boardBoss.readSpace([3, 3])).toEqual(PHARAOH);
+  expect(boardBoss.readSpace([2, 3])).toEqual(null);
+  expect(boardBoss.readSpace([3, 3]).id).toEqual(piece.id);
 });
 
 it("can't move pharaoh to where scarab is", () => {
   let boardBoss = new BoardBoss();
 
-  boardBoss.writeSpace([2, 3], PHARAOH);
-  boardBoss.writeSpace([3, 3], SCARAB);
+  const pharaoh = new PieceBoss({ type: PHARAOH, player: PLAYER_ONE });
+  const scarab = new PieceBoss({ type: SCARAB, player: PLAYER_ONE });
+  boardBoss.writeSpace([2, 3], pharaoh);
+  boardBoss.writeSpace([3, 3], scarab);
 
   boardBoss.selectPiece([2, 3]);
   expect(() => boardBoss.movePiece([2, 3], [3, 3])).toThrow(InvalidMoveError);
@@ -63,12 +65,14 @@ it("can't move pharaoh to where scarab is", () => {
 it("can move scarab to where pharaoh is and switch positions", () => {
   let boardBoss = new BoardBoss();
 
-  boardBoss.writeSpace([3, 3], SCARAB);
-  boardBoss.writeSpace([2, 3], PHARAOH);
+  const scarab = new PieceBoss({ type: SCARAB, player: PLAYER_ONE });
+  const pharaoh = new PieceBoss({ type: PHARAOH, player: PLAYER_ONE });
+  boardBoss.writeSpace([3, 3], scarab);
+  boardBoss.writeSpace([2, 3], pharaoh);
 
   boardBoss.selectPiece([3, 3]);
   boardBoss.movePiece([3, 3], [2, 3]);
 
-  expect(boardBoss.readSpace([3, 3])).toEqual(PHARAOH);
-  expect(boardBoss.readSpace([2, 3])).toEqual(SCARAB);
+  expect(boardBoss.readSpace([3, 3]).id).toEqual(pharaoh.id);
+  expect(boardBoss.readSpace([2, 3]).id).toEqual(scarab.id);
 });
